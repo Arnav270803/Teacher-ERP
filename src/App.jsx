ccef37a2cc05b8c1ce06b2f7e23a31268a59045c
Arnav Sharma <123370153+Arnav270803@users.noreply.github.com>
2025-09-02 20:25:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import Navbar from './component/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex item-center justify-between'>
+        <div className='pt-2 pl-2'>
+<lord-icon
+    src="https://cdn.lordicon.com/jeuxydnh.json"
+    trigger="hover"
+    stroke="light"
+    colors="primary:#242424,secondary:#08a88a">
+</lord-icon>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
