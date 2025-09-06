5404687a54ae39ad6badc9e7373e694178acf197
Arnav Sharma <123370153+Arnav270803@users.noreply.github.com>
2025-09-06 20:31:19 +0530

    Added landing page and starting creating langing page from here

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
+import Attendance from './Pages/Attendance'
 
 const App = () => {
   return (
     <div className="App">
+      
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/attendance" element={<Attendance />} />
       </Routes>
     </div>
+    
   )
 }
 
 export default App
+
